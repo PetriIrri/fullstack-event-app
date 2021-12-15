2751082dc9a86625debe6bff8563c43d4cc28f44
PetriIrri <petri.irri@gmail.com>
2021-12-15 16:15:50 +0200

    Add route GET"/:id" to get specific event by id.

--- routes/events.js
+++ routes/events.js
@@ -11,4 +11,19 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/:id([0-9]+$)", async (req, res) => {
+  try {
+    let data = await connection.findById(req.params.id);
+    if (data.length > 0) {
+      res.status(200).send(data);
+    } else {
+      res
+        .status(404)
+        .end(JSON.stringify({ msg: "No resource found with given id" }));
+    }
+  } catch (err) {
+    res.status(400).end(JSON.stringify(err));
+  }
+});
+
 module.exports = router;
