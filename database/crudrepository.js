cad02d5d28a39526741323726c41daeef0b68a22
PetriIrri <petri.irri@gmail.com>
2021-12-15 16:12:50 +0200

    Add findbyid(id) to find an event by given id.

--- database/crudrepository.js
+++ database/crudrepository.js
@@ -28,6 +28,26 @@ let connectionFunctions = {
     }
     return new Promise(findAll);
   },
+  findById: (id) => {
+    function findById(resolve, reject) {
+      // This will return a row of null if no record is found.
+      // It happens because of GROUP_CONCAT
+      const sql = `SELECT Events.*,
+      GROUP_CONCAT(Tags.tag_name SEPARATOR ', ') AS tags
+      FROM Events
+      INNER JOIN Event_tags ON Events.id = Event_tags.event_id
+      INNER JOIN Tags ON Event_tags.tag_id = Tags.id
+      WHERE Events.id = ?;`;
+      pool.query(sql, id, (err, location) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(location);
+        }
+      });
+    }
+    return new Promise(findById);
+  },
 };
 
 module.exports = connectionFunctions;
