bb6287fee29b1713148c01ecd9b2854443c3ef23
PetriIrri <petri.irri@gmail.com>
2021-12-15 13:14:31 +0200

    Add: crudrepository.js for databse functionality.

---
+++ database/crudrepository.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const mysql = require("mysql");
+
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DB,
+});
+
+//create database functions here
+let connectionFunctions = {
+  placeholder: "placeholder",
+};
+
+module.exports = connectionFunctions;
