98d4ccc2e7651334ef53a765514201f097b39e1d
PetriIrri <petri.irri@gmail.com>
2021-12-17 16:40:43 +0200

    Add navbar component.

---
+++ frontend/src/Navbar.js
@@ -0,0 +1,18 @@
+function Navbar(props) {
+  return (
+    <div className="navbar row">
+      <div className="col">
+        <h1>Otsikko</h1>
+      </div>
+      <div className="col">
+        <ul>
+          <li>Linkki 1</li>
+          <li>Linkki 2</li>
+          <li>Linkki 3</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
