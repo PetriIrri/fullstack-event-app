ccfc88492d90def1a22c9ce3ed88b9651b49b0ae
PetriIrri <petri.irri@gmail.com>
2021-12-28 15:44:41 +0200

    Update Map() to get markers using axios.get().
    Map() now has useState hook for map markers and useEffect()
    to get the events on components mounting using axios.get().

--- frontend/src/Map.js
+++ frontend/src/Map.js
@@ -1,18 +1,43 @@
+import { useState, useEffect } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import axios from "axios";
 
 function Map(props) {
+  const [markers, setMarkers] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      axios.get("http://localhost:8080/events/").then((response) => {
+        console.log(response.data);
+        let newMarkers = [];
+        for (let i = 0; i < response.data.length; i++) {
+          newMarkers.push(
+            <Marker
+              position={[response.data[i].latitude, response.data[i].longitude]}
+              key={response.data[i].id}
+            >
+              <Popup>event id: {response.data[i].id}</Popup>
+            </Marker>
+          );
+        }
+        setMarkers(newMarkers);
+      });
+    };
+    getData();
+  }, []);
+
   return (
     <div className="col-md-8 leaflet-container">
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <MapContainer
+        center={[61.498145583592105, 23.765849800508377]}
+        zoom={13}
+        scrollWheelZoom={true}
+      >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {markers}
       </MapContainer>
     </div>
   );
