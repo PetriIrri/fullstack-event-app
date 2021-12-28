59bb4b3f5fa50eecbd63b672ee4799236db14f9b
PetriIrri <petri.irri@gmail.com>
2021-12-28 11:09:28 +0200

    Update Navbar to better use bootstrap.

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,17 +1,26 @@
 function Navbar(props) {
   return (
-    <div className="navbar row">
-      <div className="col">
-        <h1>Otsikko</h1>
+    <nav className="navbar navbar-expand bg-info">
+      <div className="container-fluid">
+        <a className="navbar-brand text-dark" href="./">
+          EventFin
+        </a>
+        <div className=" navbar-expand">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link text-dark" aria-current="page" href="./">
+                Linkki1
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-dark" href="./">
+                Linkki2
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className="col">
-        <ul>
-          <li>Linkki 1</li>
-          <li>Linkki 2</li>
-          <li>Linkki 3</li>
-        </ul>
-      </div>
-    </div>
+    </nav>
   );
 }
 
