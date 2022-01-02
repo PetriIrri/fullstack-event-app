35757734a8f7deada3d66e6da60a6cc753440b46
PetriIrri <petri.irri@gmail.com>
2022-01-02 16:01:32 +0200

    Update marker popups to show short description.
    The popups now show short description about the event. Popup also has
    button link to view more details about the event.

--- frontend/src/Map.js
+++ frontend/src/Map.js
@@ -11,12 +11,39 @@ function Map(props) {
         console.log(response.data);
         let newMarkers = [];
         for (let i = 0; i < response.data.length; i++) {
+          const startDate = new Date(response.data[i].start_date);
+          const endDate = new Date(response.data[i].end_date);
+          // Format dates for the popup
+          let dates = `${startDate.getDate()}.${
+            startDate.getMonth() + 1
+          }.${startDate.getFullYear()}-
+          ${endDate.getDate()}.${
+            endDate.getMonth() + 1
+          }.${endDate.getFullYear()}`;
+          // Create markers for the events.
           newMarkers.push(
             <Marker
               position={[response.data[i].latitude, response.data[i].longitude]}
               key={response.data[i].id}
             >
-              <Popup>event id: {response.data[i].id}</Popup>
+              <Popup>
+                <h4>{response.data[i].event_name}</h4>
+                <ul className="list-group list-group-flush">
+                  <li className="list-group-item">
+                    {response.data[i].short_description}
+                  </li>
+                  <li className="list-group-item">{dates}</li>
+                  <li className="list-group-item">
+                    <a
+                      href={"/event/" + response.data[i].id}
+                      className="btn btn-primary text-light"
+                      role="button"
+                    >
+                      Lisätietoa
+                    </a>
+                  </li>
+                </ul>
+              </Popup>
             </Marker>
           );
         }
@@ -27,7 +54,7 @@ function Map(props) {
   }, []);
 
   return (
-    <div className="col-md-8 leaflet-container">
+    <div className="col-md-10 leaflet-container">
       <MapContainer
         center={[61.498145583592105, 23.765849800508377]}
         zoom={13}
