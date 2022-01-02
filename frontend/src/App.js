3c155a1787924f945fbb94d08cc16bc420ea1f14
PetriIrri <petri.irri@gmail.com>
2022-01-02 14:00:20 +0200

    Add BrowserRouter and routes for the app.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./Navbar.js";
 import Navbar from "./Navbar.js";
 import Map from "./Map";
@@ -5,12 +6,18 @@ import Map from "./Map";
 function App() {
   return (
     <div className="App container-fluid">
-      <div className="row">
-        <Navbar />
-      </div>
-      <div className="row justify-content-center">
-        <Map />
-      </div>
+      <BrowserRouter>
+        <div className="row">
+          <Navbar />
+        </div>
+        <div className="row justify-content-center">
+          <Routes>
+            <Route path="/" element={<Map />} />
+            <Route path="/test" element={<h1>Test</h1>} />
+            <Route path="*" element={<h1>404 page not foung</h1>} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
