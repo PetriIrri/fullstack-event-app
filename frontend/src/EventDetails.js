0b23233993728f7999fecf5783b0e1efb9505690
PetriIrri <petri.irri@gmail.com>
2022-01-05 13:45:37 +0200

    Refactor EventDetails to use EventApi and Map.js.
    EventDetails now uses EventApi fetchById(id) in useEffect to get
    the event details. fetchById(id) is sent the events id that is received
    as a parameter from url. After data is fetched it uses Map component
    to get the map and passes it the marker with the events location.

--- frontend/src/EventDetails.js
+++ frontend/src/EventDetails.js
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
 import { useParams } from "react-router-dom";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import { fetchById } from "./api/EventApi";
+import Map from "./components/Map";
 
 function EventDetails(props) {
   const { id } = useParams();
@@ -10,25 +11,25 @@ function EventDetails(props) {
 
   useEffect(() => {
     const getEvent = async () => {
-      axios.get(`http://localhost:8080/events/${id}`).then((response) => {
-        let data = response.data[0];
-        console.log(data);
-        setEvent({
-          name: data.event_name,
-          organizer: data.event_organizer,
-          url: data.event_url,
-          shortDescription: data.short_description,
-          fullDescription: data.full_description,
-          startDate: new Date(data.start_date),
-          endDate: new Date(data.end_date),
-          address: data.address,
-          city: data.city,
-          tags: data.tags,
-          latitude: data.latitude,
-          longitude: data.longitude,
-        });
-        setIsLoaded(true);
+      let response = await fetchById(id);
+      let data = response.data[0];
+      console.log(data);
+      setEvent({
+        name: data.event_name,
+        organizer: data.event_organizer,
+        url: data.event_url,
+        shortDescription: data.short_description,
+        fullDescription: data.full_description,
+        startDate: new Date(data.start_date),
+        endDate: new Date(data.end_date),
+        address: data.address,
+        city: data.city,
+        tags: data.tags,
+        latitude: data.latitude,
+        longitude: data.longitude,
+        marker: <Marker position={[data.latitude, data.longitude]}></Marker>,
       });
+      setIsLoaded(true);
     };
     getEvent();
   }, [id]);
@@ -52,36 +53,18 @@ function EventDetails(props) {
           : ""}
       </p>
       <p>{event.organizer ? event.organizer : ""}</p>
-      <a href={event.url} className="btn btn-primary text-light" role="button">
+      <a
+        href={event.url ? event.url : "/error"}
+        className="btn btn-primary text-light"
+        role="button"
+      >
         Tapahtuman järjestäjän sivut
       </a>
       <br />
       <a href="/" className="btn btn-success text-light" role="button">
         Takaisin etusivulle
       </a>
-      {isLoaded ? (
-        <MapContainer
-          center={[
-            event.latitude ? event.latitude : 61.498145583592105,
-            event.longitude ? event.longitude : 23.765849800508377,
-          ]}
-          zoom={13}
-          scrollWheelZoom={true}
-        >
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker
-            position={[
-              event.latitude ? event.latitude : 1,
-              event.longitude ? event.longitude : 1,
-            ]}
-          ></Marker>
-        </MapContainer>
-      ) : (
-        ""
-      )}
+      {isLoaded ? <Map markers={event.marker} /> : ""}
     </div>
   );
 }
