6d9033543ae046e740f368476f114909256dd875
PetriIrri <petri.irri@gmail.com>
2022-01-05 14:58:38 +0200

    Add footer.js that is the footer for web page.

---
+++ frontend/src/components/footer.js
@@ -0,0 +1,21 @@
+function Footer(props) {
+  return (
+    <footer className="footer row mt-auto fixed-bottom py-3 bg-info">
+      <div className="container col-sm-4 ">
+        <p className="text-bold text-center">Tehnyt: Petri Irri</p>
+      </div>
+      <div className="container col-sm-4 ">
+        <p className="text-bold text-center">petri.irri@gmail.com</p>
+      </div>
+      <div className="container col-sm-4 ">
+        <p className="text-bold text-center">
+          <a href="https://github.com/PetriIrri">
+            https://github.com/PetriIrri
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
