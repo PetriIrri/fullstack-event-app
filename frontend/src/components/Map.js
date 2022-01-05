4324252fa4f1467b079e4decd5337312cefdf73a
PetriIrri <petri.irri@gmail.com>
2022-01-05 12:02:09 +0200

    Refactor Map.js to make it a pure component.
    Map.js has no functionality for fetching events. This component no
    longer has any states and functions with props. Returns the map with
    all markers given as props.

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -1,58 +1,6 @@
-import { useState, useEffect } from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import axios from "axios";
+import { MapContainer, TileLayer } from "react-leaflet";
 
 function Map(props) {
-  const [markers, setMarkers] = useState([]);
-
-  useEffect(() => {
-    const getData = async () => {
-      axios.get("http://localhost:8080/events/").then((response) => {
-        console.log(response.data);
-        let newMarkers = [];
-        for (let i = 0; i < response.data.length; i++) {
-          const startDate = new Date(response.data[i].start_date);
-          const endDate = new Date(response.data[i].end_date);
-          // Format dates for the popup
-          let dates = `${startDate.getDate()}.${
-            startDate.getMonth() + 1
-          }.${startDate.getFullYear()}-
-          ${endDate.getDate()}.${
-            endDate.getMonth() + 1
-          }.${endDate.getFullYear()}`;
-          // Create markers for the events.
-          newMarkers.push(
-            <Marker
-              position={[response.data[i].latitude, response.data[i].longitude]}
-              key={response.data[i].id}
-            >
-              <Popup>
-                <h4>{response.data[i].event_name}</h4>
-                <ul className="list-group list-group-flush">
-                  <li className="list-group-item">
-                    {response.data[i].short_description}
-                  </li>
-                  <li className="list-group-item">{dates}</li>
-                  <li className="list-group-item">
-                    <a
-                      href={"/event/" + response.data[i].id}
-                      className="btn btn-primary text-light"
-                      role="button"
-                    >
-                      Lisätietoa
-                    </a>
-                  </li>
-                </ul>
-              </Popup>
-            </Marker>
-          );
-        }
-        setMarkers(newMarkers);
-      });
-    };
-    getData();
-  }, []);
-
   return (
     <div className="col-md-10 leaflet-container">
       <MapContainer
@@ -64,7 +12,7 @@ function Map(props) {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {markers}
+        {props.markers}
       </MapContainer>
     </div>
   );
